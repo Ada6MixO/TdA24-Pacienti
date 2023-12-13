2dcce25a98f2103ad380b62fba4a8b1871c0bd4c
Coolheim <144121971+Coolheim@users.noreply.github.com>
2023-12-13 21:46:24 +0100

    Delete django-boilerplate directory

--- django-boilerplate/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-
-def api_view(request):
-    data = {"secret": "The cake is a lie"}
-    return JsonResponse(data)
-
-def index(request):
-    return render(request, 'index.html')
