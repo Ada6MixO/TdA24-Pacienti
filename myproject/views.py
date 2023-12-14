7fe189e7f4b9c7a1664ecf5649e7bed1c28e63d5
Ada6MixO <144122037+Ada6MixO@users.noreply.github.com>
2023-12-14 14:19:52 +0100

    Update views.py

--- myproject/views.py
+++ myproject/views.py
@@ -2,8 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 def lecturer_view(request):
-    data = {"secret": "The cake is a lie"}
-    return JsonResponse(data)
+    return render(request, 'index.html')
 
 def index(request):
     return render(request, 'index.html')
